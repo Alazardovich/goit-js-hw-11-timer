442eebe547034d4d94eaef0f3b5d363c2b43c918
Artem <lazarazuy@gmail.com>
2021-10-22 22:50:49 +0200

    add timer

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ class CountdownTimer {
     start() {
         // console.log(document.querySelectorAll('.value'));
     //    document.querySelectorAll('.value').textContent =  this.changeNumberText(0);
-    const intervalId = setInterval(()=> {
+    this.changeNumberText(this.updateTimesClock(0));
+    setInterval(()=> {
         const currentTime = Date.now();
         const timeDifference = this.targetDate - currentTime;
         const time = this.updateTimesClock(timeDifference);
         this.changeNumberText(time);
-    },1000)
+    },1000);
     }
     updateTimesClock(time) {
         const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
@@ -23,7 +24,7 @@ class CountdownTimer {
         const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
         const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
         return ({days, hours, mins, secs});
-    };
+    }
     changeNumberText({days, hours, mins, secs }) {
         const day = document.querySelector('span[data-value="days"]');
         const hour = document.querySelector('span[data-value="hours"]');
@@ -33,12 +34,12 @@ class CountdownTimer {
         day.textContent = `${days}`;
         hour.textContent = `${hours}`;
         min.textContent = `${mins}`;
-        sec.textContent = `${secs}`
+        sec.textContent = `${secs}`;
     
     }
     pad(value) {
         return String(value).padStart(2,'0');
-    };
+    }
     };
     const timer = new CountdownTimer({
         selector: '#timer-1',
